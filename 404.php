81394232a87d1d015b39794c8990ba3173abb968
Emerling <emerling2001@gmail.com>
2020-09-07 12:41:32 -0500

    culminado los arreglos css
    actualizado el 4040.php con el css

---
+++ 404.php
@@ -0,0 +1,20 @@
+<?php get_header();?>
+
+
+<div class="container mb-5 mt-5">
+
+    <div class="row align-items-center">
+        <div class="col-lg-6">
+            <img src="<?php bloginfo('template_url'); ?>/images/404.png" class="img-fluid">
+            <br><br>
+        </div>
+        <div class="col-lg-6 font-weight-light">
+
+            <h1 class="font-weight-light text-black">404: Página no encontrada.
+            </h1>
+            <p>Lo sentimos, no pudimos encontrar esa página.</p>
+        </div>
+    </div>
+</div>
+<hr>
+<?php get_footer(); ?>
